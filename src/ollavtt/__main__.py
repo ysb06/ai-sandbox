2c92979e20d9419296e50ffab89e950a3396658d
Seungbin Yim <ysb06@hotmail.com>
2026-09-16 10:04:33 +0900

    Add iter_frame_batches

--- src/ollavtt/__main__.py
+++ src/ollavtt/__main__.py
@@ -1,6 +1,16 @@
 import argparse
-from typing import Sequence
+from fractions import Fraction
+from math import isfinite
+from typing import Iterator, Sequence, Union
 import av
+from dataclasses import dataclass
+
+
+@dataclass
+class FrameBatch:
+    frame_id: int
+    frame_time: float
+    frame: av.VideoFrame
 
 
 def parse_args(argv: Sequence[str] | None = None) -> argparse.Namespace:
@@ -12,14 +22,51 @@ def parse_args(argv: Sequence[str] | None = None) -> argparse.Namespace:
 
     return parser.parse_args(argv)
 
-def extract_frames(file_path: str, interval: float = 1) -> list:
-    frame_batch = []
+
+# Todo: 현재는 프레임 배치만 반환하지만 frame_id, frame_time, frame 튜플 배치 반환으로 변경해야 함
+def iter_frame_batches(
+    file_path: str,
+    start_time: float = 0,
+    interval: float = 1,
+    batch_size: int = 8,
+) -> Iterator[list[FrameBatch]]:
+    sample_interval = Fraction(str(interval))
+    next_sample_time = Fraction(0)
+
+    start_time_f = Fraction(str(start_time))
+    frame_batch: list[FrameBatch] = []
     with av.open(file_path) as container:
-        for frame in container.decode(video=0):
-            frame_batch.append(frame)
-    
-    return frame_batch
-    
+        for idx, frame in enumerate(container.decode(video=0)):
+            if frame.pts is None or frame.time_base is None:
+                raise ValueError("Cannot sample a video frame without a timestamp")
+
+            timestamp = frame.pts * frame.time_base
+            if start_time_f is None:
+                start_time_f = timestamp
+            elapsed = timestamp - start_time_f
+            if elapsed < next_sample_time:
+                continue
+
+            frame_batch.append(
+                FrameBatch(
+                    frame_id=idx,
+                    frame_time=float(elapsed),
+                    frame=frame,
+                )
+            )
+            next_sample_time = (elapsed // sample_interval + 1) * sample_interval
+            if len(frame_batch) == batch_size:
+                yield frame_batch
+                frame_batch = []
+
+    if frame_batch:
+        yield frame_batch
+
+
+
+def convert_frame_batch_to_image_batch(frames: list[FrameBatch]) -> list:
+    pass
+
 
 if __name__ == "__main__":
     args = parse_args()
